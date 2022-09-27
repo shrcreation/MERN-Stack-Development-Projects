8a756e420833425567507c0349512fe43f10be0a
shrCreation <shrhossain786@gmail.com>
2022-09-28 00:50:50 +0600

    start the movies project

--- JavaScript/Normal_Practice/DOM/assets/scripts/app.js
+++ JavaScript/Normal_Practice/DOM/assets/scripts/app.js
@@ -1,10 +1,36 @@
-const addMoviesBtn = document.getElementById('addMoviesBtn');
+//global constant variable
+const addMoviesModal = document.getElementById('add-modal');
+const startMoviesBtn = document.querySelector('header button');
+//
+const mTitle = document.getElementById('title');
+const mDescription = document.getElementById('description');
+const mYear = document.getElementById('year');
+const mDuration = document.getElementById('duration');
+const mGenres = document.getElementById('genres');
+const mImage = document.getElementById('image-url');
+const mRating = document.getElementById('rating');
+const mTrailerLink = document.getElementById('trailer-link');
+const mMovieLink = document.getElementById('movie-link');
 
+const backdrop = document.getElementById('backdrop');
+const cancelMovieModal = addMoviesModal.querySelector('.btn--passive');
 
-const showModal = () => {
-    let modal = document.getElementById('add-modal');
-    modal.style.display = 'block';
-}
+const toggleBackdrop = () => {
+  backdrop.classList.toggle('visible');
+};
+const toggleMovieModal = () => {
+  addMoviesModal.classList.toggle('visible');
+  toggleBackdrop();
+};
 
+const backdropClickHandler = () => {
+  toggleMovieModal();
+};
 
-addMoviesBtn.addEventListener('click',showModal);
+const cancelMovie = () => {
+  toggleMovieModal();
+};
+
+startMoviesBtn.addEventListener('click', toggleMovieModal);
+backdrop.addEventListener('click', backdropClickHandler);
+cancelMovieModal.addEventListener('click', cancelMovie);
