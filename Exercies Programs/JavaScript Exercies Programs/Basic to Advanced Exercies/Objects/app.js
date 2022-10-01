10ef6fe3b169f19832bda7de74a6270da8beeb87
shrCreation <shrhossain786@gmail.com>
2022-10-01 16:18:20 +0600

    create a dynamic key value pair objects

--- Exercies Programs/JavaScript Exercies Programs/Basic to Advanced Exercies/Objects/app.js
+++ Exercies Programs/JavaScript Exercies Programs/Basic to Advanced Exercies/Objects/app.js
@@ -1,1 +1,26 @@
+const addMoviesBtn = document.getElementById('add-movie-btn');
+const searchBtn = document.getElementById('search-btn');
 
+const addMovieHandler = () => {
+  const movieTitle = document.getElementById('title').value;
+  const extraName = document.getElementById('extra-name').value;
+  const extraValue = document.getElementById('extra-value').value;
+
+  if (
+    movieTitle.trim() === '' ||
+    extraName.trim() === '' ||
+    extraValue.trim() === ''
+  ) {
+    return alert('Fields are required!!!');
+  }
+
+  const newMovies = {
+    id: Math.random(),
+    info: {
+      movieTitle, //key name and value name are same so use 1
+      [extraName]: extraValue, //dynamic key name and dynamic value
+    },
+  };
+}; //ends addMovieHandler
+
+addMoviesBtn.addEventListener('click', addMovieHandler);
