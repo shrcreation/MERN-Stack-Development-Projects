8ac2a26dd2882e5238cc6f272bf005d182327c91
shrCreation <shrhossain786@gmail.com>
2022-10-22 09:34:16 +0600

    react component

--- Exercise Programs/React JS Exercies Programs/first-react-app/src/components/ExpenseItems.js
+++ Exercise Programs/React JS Exercies Programs/first-react-app/src/components/ExpenseItems.js
@@ -1,5 +1,18 @@
-function ExpenseItem(){
-    return <h2>Expense Items</h2>
+import './ExpenseItems.css';
+
+function ExpenseItem() {
+  const expenseDate = new Date(2022, 9, 22);
+  const expenseTitle = 'Car Insurance';
+  const expenseAmmount = 249.99;
+  return (
+    <div className="expense-item">
+      <div>{expenseDate.toISOString}</div>
+      <div className="expense-item__description">
+        <h2>{expenseTitle}</h2>
+        <div className="expense-item__price">${expenseAmmount}</div>
+      </div>
+    </div>
+  );
 }
 
 export default ExpenseItem;
