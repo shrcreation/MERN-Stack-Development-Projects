f03dfa31ba23a967fff710acdc2d0ccc1640dac2
shrCreation <shrhossain786@gmail.com>
2022-10-23 09:23:26 +0600

    props

--- Exercise Programs/React JS Exercies Programs/first-react-app/src/App.js
+++ Exercise Programs/React JS Exercies Programs/first-react-app/src/App.js
@@ -6,38 +6,45 @@ function App() {
       id: 'e1',
       title: 'Toilet Paper',
       amount: 94.12,
+      date: new Date(2022, 5, 8),
     },
-    { id: 'e2', title: 'New TV', amount: 799.49, },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2022, 7, 21) },
     {
       id: 'e3',
       title: 'Car Insurance',
       amount: 294.67,
+      date: new Date(2022,3,21),
     },
     {
       id: 'e4',
       title: 'New Desk (Wooden)',
       amount: 450,
+      date: new Date(2022,9,24),
     },
   ];
   return (
     <div>
       <h2>Let's get started!</h2>
-      <ExpenseItems title={expenses[0].title}
-        amount={expenses[0].amount}>
-        
-      </ExpenseItems>
-      <ExpenseItems title={expenses[1].title}
-        amount={expenses[1].amount}>
-        
-      </ExpenseItems>
-      <ExpenseItems title={expenses[2].title}
-        amount={expenses[2].amount}>
-        
-      </ExpenseItems>
-      <ExpenseItems title={expenses[3].title}
-        amount={expenses[3].amount}>
-        
-      </ExpenseItems>
+      <ExpenseItems
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      ></ExpenseItems>
+      <ExpenseItems
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      ></ExpenseItems>
+      <ExpenseItems
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      ></ExpenseItems>
+      <ExpenseItems
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      ></ExpenseItems>
     </div>
   );
 }
